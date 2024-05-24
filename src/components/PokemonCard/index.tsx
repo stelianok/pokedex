6903cd0b30c5ad51467956f882134431fb5885d2
Kauã Steliano <stelianok@gmail.com>
2024-05-24 13:42:49 -0300

    Created structure of pokemon card

---
+++ src/components/PokemonCard/index.tsx
@@ -0,0 +1,12 @@
+import Ditto from "../../assets/Ditto.png";
+
+export function PokemonCard() {
+  return (
+    <div>
+      <p>#132</p>
+      <img src={Ditto} />
+      <p>Ditto</p>
+      <div></div>
+    </div>
+  )
+}
