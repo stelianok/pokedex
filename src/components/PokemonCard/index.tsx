0b7d4a6602e213c70c9e127a29744b0ee106522e
Kauã Steliano <stelianok@gmail.com>
2024-05-24 18:25:21 -0300

    Created Basic styles for pokemon card

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,12 +1,17 @@
 import Ditto from "../../assets/Ditto.png";
 
+import './styles.css';
+
 export function PokemonCard() {
   return (
-    <div>
-      <p>#132</p>
-      <img src={Ditto} />
-      <p>Ditto</p>
-      <div></div>
+    <div className="pokemonCardContainer">
+      <p className="pokemonNumber">#132</p>
+      <img
+        src={Ditto}
+        className="pokemonAvatar"
+      />
+      <p className="pokemonName">Ditto</p>
+      {/* <div className="grayDiv"></div> */}
     </div>
   )
 }
