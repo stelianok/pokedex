dccd0fb0b2ea2dfbf19686937adb9a1777123072
Kauã Steliano <stelianok@gmail.com>
2024-05-24 15:13:20 -0300

    Implemented SearchBar textfield styles, added button to searchBar component

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,21 +1,27 @@
 import { SearchOutlined } from "@mui/icons-material";
-import { InputAdornment, TextField } from "@mui/material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
 
 import "./styles.css";
+import Tag from "../icons/tag";
 
 export function SearchBar() {
   return (
-    <div className="SearchBarContainer">
+    <div className="container">
       <TextField
+        placeholder="Search"
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
-              <SearchOutlined />
+              <SearchOutlined style={{ fontSize: 16, color: "#DC0A2D" }} />
             </InputAdornment>
           ),
         }}
         variant="outlined"
+        className="searchBar"
       />
+      <IconButton aria-label="filter pokemons">
+        <Tag size={"24"} color={"#fff"} />
+      </IconButton>
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,21 @@
 import './styles.css';
 
-import { IconButton } from '@mui/material';
-
 import { PokemonCard } from '../../components/PokemonCard';
 import { SearchBar } from '../../components/SearchBar';
 
 import Pokeball from '../../components/icons/pokeball';
-import Tag from '../../components/icons/tag';
 
 export function Home() {
   return (
     <>
       <div className="background">
         <header>
-          <div className="LogoContainer">
+          <div className="logoContainer">
             <Pokeball size={"24"} color={"#fff"} />
             <h1>Pokedéx</h1>
           </div>
           <div>
             <SearchBar />
-            <IconButton aria-label="filter pokemons">
-              <Tag size={"48"} color={"#fff"} />
-            </IconButton>
           </div>
         </header >
         <main>
