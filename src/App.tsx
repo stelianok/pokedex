d45697871831dc0d4e97613165ea70943fadcc81
Kauã Steliano <stelianok@gmail.com>
2024-05-28 09:23:34 -0300

    Installed and configured react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import { Home } from './pages/Home';
+import { PokemonDetailed } from './pages/PokemonDetailed';
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "pokemons/:pokemonId",
+    element: <PokemonDetailed />
+  }
+])
 export default function App() {
   return (
-    <Home />
+    <RouterProvider router={router} />
   )
 }
